697089ba834e2c7ee45bba6527568a20ab413787
anonhacker47 <rubeenbilal47@gmail.com>
2024-02-29 17:51:44 +0530

    [WIP] Series page redesign

--- vertigo-ui/src/App.vue
+++ vertigo-ui/src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { useUserStore } from "./store/user";
-import AuthenticationService from "./services/AuthenticationService";
-import { onMounted } from "vue";
+
 const store = useUserStore();
 async function refreshToken() {
   const data = {
