da021f9f5ee44a5270dfb9a648d47bda3f551eb7
anonhacker47 <rubeenbilal47@gmail.com>
2024-07-19 11:43:47 +0530

    fix(api): single select combobox for publisher and main_char fields

--- vertigo-ui/src/components/customInputs/SingleSelectCombobox.vue
+++ vertigo-ui/src/components/customInputs/SingleSelectCombobox.vue
@@ -1,49 +1,41 @@
 <template>
-  <Combobox v-model="model">
-    <div class="relative w-52">
+  <Combobox v-model="selected" nullable>
+    <div class="relative max-w-52">
       <div class="relative w-full cursor-default rounded-lg bg-base-10">
         <ComboboxInput class="w-full input input-bordered" autoComplete="off" @change="query = $event.target.value"
           :placeholder="placeholder" />
         <ComboboxButton class="absolute inset-y-0 right-0 flex items-center pr-2">
           <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
         </ComboboxButton>
-      </div>
+      </div>  
       <TransitionRoot leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0"
         @after-leave="query = ''">
         <ComboboxOptions
-          class="absolute mt-1 max-h-60 w-full dropdown-content overflow-auto rounded-md bg-base-100 py-1 shadow-lg ring-2 ring-gray-400/5 focus:outline-none sm:text-sm"
+          class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-base-100 py-1 shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm"
           style="z-index: 1;">
-          <ComboboxOption v-for="item in filteredItems" as="template" :key="item.id" :value="item.value"
-            v-slot="{ active, selected }">
+          <ComboboxOption v-for="person in filteredPeople" as="template" :key="person.id" :value="person.value"
+            v-slot="{ active }">
             <ul class="">
-              <li class="relative cursor-pointer select-none py-3 pl-10 pr-4" :class="{
-                'bg-base-300 text-teal-400': active,
-                'text-white': !active,
-              }">
-                <span class="block truncate" :class="{ 'font-medium': active, 'font-normal': !active }">
-                  {{ item.value }}
-                </span>
-                <span v-if="selected" class="absolute inset-y-0 left-0 flex items-center pl-3"
-                  :class="{ 'text-teal-400': active, 'text-white': !active }">
-                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
-                </span>
-              </li>
-            </ul>
-          </ComboboxOption>
-          <ComboboxOption v-slot="{ active, selected }" v-if="queryItem" :value="queryItem"  class="relative cursor-pointer">
-            <ul>
               <li class="relative cursor-default select-none py-3 pl-10 pr-4" :class="{
                 'bg-base-300 text-white': active,
                 'text-white': !active,
               }">
                 <span class="block truncate" :class="{ 'font-medium': active, 'font-normal': !active }">
-                  {{ query }}
-                </span>
-                <span v-if="selected" class="absolute inset-y-0 left-0 flex items-center pl-3"
-                  :class="{ 'text-white': active, 'text-teal-600': !active }">
-                  <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                  {{ person.value }}
                 </span>
               </li>
+            </ul>
+          </ComboboxOption>
+          <ComboboxOption v-slot="{ active }" v-if="queryPerson" :value="queryPerson" class="relative cursor-default">
+            <ul class="">
+            <li class="relative cursor-default select-none py-3 pl-10 pr-4" :class="{
+              'bg-base-300 text-white': active,
+              'text-white': !active,
+            }">
+              <span class="block truncate" :class="{ 'font-medium': active, 'font-normal': !active }">
+                Create "{{ query }}"
+              </span>
+            </li>
             </ul>
           </ComboboxOption>
         </ComboboxOptions>
@@ -57,29 +49,25 @@ import { ref, computed, onMounted } from 'vue'
 import {
   Combobox,
   ComboboxInput,
-  ComboboxButton,
   ComboboxOptions,
+  ComboboxButton,
   ComboboxOption,
   TransitionRoot,
 } from '@headlessui/vue'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/20/solid'
-import SeriesService from '@/services/SeriesService';
+import SeriesService from 'src/services/SeriesService';
 
-const items = ref("")
+const people = ref([])
 const props = defineProps({
   field: String,
   placeholder: String
 })
 
-const model = defineModel({
-  type: Array,
-  default: []
-})
+let selected = ref([])
 
 onMounted(() => {
   getSeriesFields();
 });
-
 
 async function getSeriesFields() {
   try {
@@ -87,46 +75,29 @@ async function getSeriesFields() {
       `${props.field}`,
     );
 
-    items.value = response.data;
+    people.value = response.data;
 
-    console.log("items2multiple",items.value);
+    console.log(people.value);
   } catch (error) {
     console.log(error);
   }
 }
 let query = ref('')
 
-const queryItem = computed(() => {
+const queryPerson = computed(() => {
   return query.value === '' ? null : query.value
 })
 
-let filteredItems = computed(() =>
+let filteredPeople = computed(() =>
   query.value === ''
-    ? items.value
-    : items.value.filter((item) =>
-      item.value
+    ? people.value
+    : people.value.filter((person) =>
+      person.value
         .toLowerCase()
         .replace(/\s+/g, '')
         .includes(query.value.toLowerCase().replace(/\s+/g, ''))
     )
 )
 
-// const selectedValuesPlaceholder = computed(() => {
-//   return model.value.length > 0 ? model.value.join(',= : props.placeholder;
-// })
 
-const removeItem = () => {
-  model.value = "";
-  console.log("model: ", model.value);
-}
-
-const addCustomItem = (value) => {
-  if (!items.value.find(item => item.value === value)) {
-    items.value.push({ id: value, value: value });
-  }
-  if (!model.value.includes(value)) {
-    model.value.push(value);
-  }
-  query.value = '';
-}
 </script>
