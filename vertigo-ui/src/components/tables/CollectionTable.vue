8dd1b842165829ae42c9d3cd54d4004561c29922
anonhacker47 <rubeenbilal47@gmail.com>
2024-08-07 17:42:33 +0530

    feat(CollectionView): add PrimeVue Table component

---
+++ vertigo-ui/src/components/tables/CollectionTable.vue
@@ -0,0 +1,36 @@
+<template>
+	<DataTable class="" :value="cards" :paginator="true" :rows="10">
+		<Column field="image_url" header="Image">
+			<template #body="{ data }">
+				<img :src="SeriesService.getImagebyId(data.id)" class="h-30 w-20" />
+			</template>
+		</Column>
+		<Column field="title" header="Title" />
+		<Column field="series_format" header="Format" />
+		<Column field="timestamp" header="Date Added" />
+		<Column field="rating" header="Rating">
+			<template #body="{ data }">
+				<Rating :modelValue="data.rating" :readonly="true" :stars="5" :cancel="false" />
+			</template>
+		</Column>
+		<Column field="owned_count" header="Owned Count" />
+		<Column field="read_count" header="Read Count" />
+	</DataTable>
+</template>
+  
+
+<script setup>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import { ref, defineProps } from 'vue';
+import SeriesService from '../../services/SeriesService';
+import Rating from 'primevue/rating'
+
+const props = defineProps({
+	cards: {
+		type: Array,
+		required: true
+	}
+})
+
+</script>
--- vertigo-ui/src/main.js
+++ vertigo-ui/src/main.js
@@ -4,18 +4,67 @@ import { createPinia } from 'pinia'
 import router from "./router";
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { plugin, defaultConfig } from '@formkit/vue';
+import PrimeVue from 'primevue/config';
 import '@formkit/themes/genesis';
 import 'animate.css'
 import './input.css'
-
+import Lara from '@primevue/themes/lara';
+import { definePreset } from '@primevue/themes';
 import "./assets/main.css";
 
 const app = createApp(App);
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate)
 
+
+const MyPreset = definePreset(Lara, {
+    semantic: {
+        colorScheme: {
+            light: {
+                surface: {
+                    0: '#ffffff',
+                    50: '{zinc.50}',
+                    100: '{zinc.100}',
+                    200: '{zinc.200}',
+                    300: '{zinc.300}',
+                    400: '{zinc.400}',
+                    500: '{zinc.500}',
+                    600: '{zinc.600}',
+                    700: '{zinc.700}',
+                    800: '{zinc.800}',
+                    900: '{zinc.900}',
+                    950: '{zinc.950}'
+                }
+            },
+            dark: {
+                surface: {
+                    0: '#ffffff',
+                    50: '{slate.50}',
+                    100: '{slate.100}',
+                    200: '{slate.200}',
+                    300: '{slate.300}',
+                    400: '{slate.400}',
+                    500: '{slate.500}',
+                    600: '{slate.600}',
+                    700: '{slate.700}',
+                    800: '{slate.800}',
+                    900: '{slate.900}',
+                    950: '{slate.950}'
+                }
+            }
+        }
+    }
+});
+
 app.use(pinia)
 app.use(router);
-app.use(plugin,defaultConfig);
+app.use(plugin, defaultConfig);
+app.use(PrimeVue, {
+    // unstyled: true,
+    theme: {
+        preset: MyPreset
+    }
+    // pt: Aura
+});
 app.mount("#app");
 
