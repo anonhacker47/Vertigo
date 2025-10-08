5f689b2b0050a3ee56427f420d0e555f3d6db36d
anonhacker47 <rubeenbilal47@gmail.com>
2025-10-08 20:26:13 +0530

    refactor<SignInView>: reduce background grid count

--- vertigo-ui/src/views/LoginView.vue
+++ vertigo-ui/src/views/LoginView.vue
@@ -1,19 +1,35 @@
 <template>
-  <div class="background-container bg-cover bg-no-repeat"
-    :style="{ backgroundImage: showDefaultWall ? 'url(' + img + ')' : '' }">
-    <div class="background-images" :class="!showDefaultWall ? 'bg-base-100' : 'bg-none'">
-      <div class="transform-class -skew-y-6 -translate-y-20 backdrop-blur-lg backdrop-brightness-50">
-        <TransitionGroup enter-active-class="animate__animated animate__flipInX"
-          leave-active-class="animate__animated animate__fadeOut">
+  <div
+    class="background-container bg-cover bg-no-repeat"
+    :style="{ backgroundImage: showDefaultWall ? 'url(' + img + ')' : '' }"
+  >
+    <div
+      class="background-images"
+      :class="!showDefaultWall ? 'bg-base-100' : 'bg-none'"
+    >
+      <div
+        class="transform-class -skew-y-6 -translate-y-20 backdrop-blur-lg backdrop-brightness-50"
+      >
+        <TransitionGroup
+          enter-active-class="animate__animated animate__flipInX"
+          leave-active-class="animate__animated animate__fadeOut"
+        >
           <div v-for="(image, index) in images" :key="index">
-            <img :src="image" class="background-image" alt="comicbooks tile backdrop" />
+            <img
+              :src="image"
+              class="background-image"
+              alt="comicbooks tile backdrop"
+            />
           </div>
         </TransitionGroup>
       </div>
       <div
-        class="w-fit h-fit card top-0 right-0 left-0 bottom-0 m-auto absolute z-20 w-md rounded-xl shadow-md bg-base-100 ">
+        class="w-fit h-fit card top-0 right-0 left-0 bottom-0 m-auto absolute z-20 w-md rounded-xl shadow-md bg-base-100"
+      >
         <div class="card-body mx-8 mx-4">
-          <h1 class="text-xl text-center font-bold card-title leading-tight tracking-tight md:text-2xl mb-2 text-white">
+          <h1
+            class="text-xl text-center font-bold card-title leading-tight tracking-tight md:text-2xl mb-2 text-white"
+          >
             Sign in to your account
           </h1>
           <form @submit.prevent="login" class="flex gap-4 flex-col">
@@ -21,25 +37,40 @@
               <label for="username" class="label">
                 <span class="label-text text-white">Username</span>
               </label>
-              <input type="text" placeholder="Username" v-model="username" id="username"
-                class="input input-bordered w-full" />
+              <input
+                type="text"
+                placeholder="Username"
+                v-model="username"
+                id="username"
+                class="input input-bordered w-full"
+              />
             </div>
             <div class="form-control">
               <label for="password" class="label">
                 <span class="label-text text-white">Password</span>
               </label>
-              <input type="password" v-model="password" id="password" class="input input-bordered w-full" />
+              <input
+                type="password"
+                v-model="password"
+                id="password"
+                class="input input-bordered w-full"
+              />
             </div>
-            <div v-if="message" class="error py-3 text-red-500">{{ message }}</div>
-            <button type="submit" class="btn btn-primary mt-4 w-full">Login</button>
+            <div v-if="message" class="error py-3 text-red-500">
+              {{ message }}
+            </div>
+            <button type="submit" class="btn btn-primary mt-4 w-full">
+              Login
+            </button>
           </form>
           <p class="text-sm font-light text-gray-500 dark:text-gray-400 mt-4">
             Don’t have an account yet?
-            <RouterLink to="/register" class="text-blue-500 underline">Sign up</RouterLink>
+            <RouterLink to="/register" class="text-blue-500 underline"
+              >Sign up</RouterLink
+            >
           </p>
         </div>
       </div>
-
     </div>
   </div>
 </template>
@@ -51,11 +82,11 @@ import { useRouter } from "vue-router";
 import AuthenticationService from "../services/AuthenticationService";
 import SeriesService from "../services/SeriesService";
 import img from "../assets/logo.svg";
-import InputText from 'primevue/inputtext'
-import Password from 'primevue/password'
-import FloatLabel from 'primevue/floatlabel'
-import Button from 'primevue/button'
-import Card from 'primevue/card'
+import InputText from "primevue/inputtext";
+import Password from "primevue/password";
+import FloatLabel from "primevue/floatlabel";
+import Button from "primevue/button";
+import Card from "primevue/card";
 
 const userStore = useUserStore();
 const route = useRouter();
@@ -67,7 +98,10 @@ const showDefaultWall = ref(false);
 
 async function login() {
   try {
-    const response = await AuthenticationService.login(username.value, password.value);
+    const response = await AuthenticationService.login(
+      username.value,
+      password.value,
+    );
     userStore.addToken(response.data.access_token);
 
     if (localStorage.getItem("token")) {
@@ -111,7 +145,7 @@ const fetchImages = async () => {
   } catch (error) {
     console.error("Error fetching images:", error);
   }
-}
+};
 
 onMounted(() => {
   fetchImages();
@@ -140,9 +174,9 @@ onMounted(() => {
   width: 100%;
   display: grid;
   grid-auto-flow: dense;
-  grid-template-columns: repeat(12, 2fr);
+  grid-template-columns: repeat(8, 2fr);
   grid-auto-rows: 20rem;
-  margin-top: -4rem
+  margin-top: -4rem;
 }
 
 .transform-class::after {
@@ -161,7 +195,6 @@ onMounted(() => {
 .background-image {
   height: 20rem;
   width: 13.5rem;
-  padding: .2rem;
+  padding: 0.2rem;
 }
-
 </style>
